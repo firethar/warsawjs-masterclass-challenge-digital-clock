5f9a4ca6a5e6a7a95d73d4a9876c20e00c32ccf9
wblazejewski <firethar@gmail.com>
2019-03-09 21:40:19 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DigitalClockComponent } from './digital-clock/digital-clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigitalClockComponent
   ],
   imports: [
     BrowserModule
